95aa336df1901bfffcc43edcf84042976afdcf8a
Harmandeep <just.h4harman@gmail.com>
2024-08-31 17:58:37 +0530

    filament updated and css styles added.

--- resources/views/filament/pages/attendance.blade.php
+++ resources/views/filament/pages/attendance.blade.php
@@ -21,7 +21,7 @@
             <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 <form class="max-w-sm mx-auto">
                     <label for="dateinput" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Date</label>
-                    <input wire:model.live="selectedDate" type="date" id="dateinput" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <input wire:model.live.debounce.500ms="selectedDate" type="date" id="dateinput" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </form>
             </div>
         </div>
