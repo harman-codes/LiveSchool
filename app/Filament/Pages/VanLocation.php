af934a6a5322d6902ac00175cfc79e6901c6abd5
Harmandeep <just.h4harman@gmail.com>
2024-09-11 23:48:42 +0530

    Location tracking added in admin panel.

---
+++ app/Filament/Pages/VanLocation.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Helpers\DT;
+use App\Models\Driver;
+use App\Models\MapKey;
+use Filament\Pages\Page;
+
+class VanLocation extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-map-pin';
+
+    protected static string $view = 'filament.pages.van-location';
+    public $selectedVan;
+    public $datetime;
+    public $location;
+    public $switch;
+    public $driverName;
+    public $vehicleNumber;
+    public $dateTime;
+    public ?bool $locationStatus;
+
+    public function getVanNumbers()
+    {
+        return Driver::query()->get(['id', 'van']);
+    }
+
+
+
+    public function render(): \Illuminate\Contracts\View\View
+    {
+
+        /*if van is not selected, set default location*/
+        if(!empty($this->selectedVan)){
+            $data = Driver::where('id', $this->selectedVan)->first();
+            $this->location = $data->location;
+            $this->driverName = $data->name;
+            $this->vehicleNumber = $data->van;
+            $this->dateTime = DT::formatDate($data->datetime, 'd-m-Y (h:i:s A)');
+            $this->locationStatus = (bool)$data->is_switchon;
+        }else{
+            $this->location = 'india';
+            $this->reset('selectedVan', 'driverName', 'vehicleNumber', 'locationStatus', 'dateTime');
+        }
+
+
+
+        return parent::render();
+    }
+
+
+}
---
+++ resources/views/filament/pages/van-location.blade.php
@@ -0,0 +1,62 @@
+@php use App\Helpers\Config; @endphp
+<x-filament-panels::page>
+<div class="ls-cards-holder">
+    <div class="ls-card">
+        <div class="ls-card-body">
+            <form class="max-w-sm mx-auto">
+                <label for="vehicle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select</label>
+                <select wire:model.change="selectedVan" id="vehicle" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="0" selected>Vehicle No</option>
+                    @if(!empty($this->getVanNumbers() ))
+                        @foreach($this->getVanNumbers() as $vanNumber)
+                            <option wire:key="{{ $vanNumber->id }}" value="{{ $vanNumber->id }}">{{ $vanNumber->van }} </option>
+                        @endforeach
+                    @endif
+                </select>
+            </form>
+        </div>
+    </div>
+
+    <div class="ls-card">
+        <div class="ls-card-body">
+            <p>Driver : {{$driverName}}</p>
+            <p>Vehicle No : {{$vehicleNumber}}</p>
+            <p>Location : @if(!empty($selectedVan)) @if($locationStatus) ON @else OFF @endif @else @endif</p>
+            <p>Updated : @if(!empty($selectedVan)) @if(!empty($dateTime)) {{$dateTime}} @else @endif @else @endif</p>
+        </div>
+    </div>
+</div>
+
+
+    @if(!empty(Config::mapkey()))
+            <div class="grid grid-cols-1 p-4 rounded-lg bg-white">
+                <iframe
+                    width="100%"
+                    height="600"
+                    style="border:0"
+                    loading="lazy"
+                    allowfullscreen
+                    referrerpolicy="no-referrer-when-downgrade"
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-lD0y-D-RVFPw_PF1TOK3P10k8lyErBs&q={{$location}}&zoom=20">
+                </iframe>
+            </div>
+    @else
+        <div class="bg-white p-4 flex justify-center items-center rounded-lg shadow-sm dark:bg-gray-800 dark:text-white">
+            Please enter Google Map key in .env file to get the location on map.
+        </div>
+    @endif
+
+    @script
+    <script>
+        document.addEventListener('livewire:navigated', () => {
+           setInterval(function(){
+               $wire.$refresh();
+           },1000*30);
+        });
+    </script>
+    @endscript
+
+
+
+
+</x-filament-panels::page>
