dd8c892c2f88bf175bc46b936d4b0e27e6d85d3f
Harmandeep <just.h4harman@gmail.com>
2024-09-28 13:38:22 +0530

    same.

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,7 @@
 
 namespace App\Models;
 
-use App\Helpers\SessionYears;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
