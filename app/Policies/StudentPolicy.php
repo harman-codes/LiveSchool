23a3174ff3db5ed3676b19c71861b1a4af3eb014
Harmandeep <just.h4harman@gmail.com>
2024-10-16 20:28:15 +0530

    Policies added for permissions

---
+++ app/Policies/StudentPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Helpers\Role;
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class StudentPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Student $student): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return Role::isAdminOrPrincipalOrManager();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Student $student): bool
+    {
+        return Role::isAdminOrPrincipalOrManager();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Student $student): bool
+    {
+        return Role::isAdminOrPrincipalOrManager();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Student $student): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Student $student): bool
+    {
+        //
+    }
+}
