ec7205ccd0b8754dd4a59f4f0329a1434035a39b
Harmandeep <just.h4harman@gmail.com>
2024-10-26 13:44:55 +0530

    updated

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\LoginAdmin;
 use App\Filament\Widgets\CalendarWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
