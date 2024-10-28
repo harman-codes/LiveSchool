3d33728790a7d48981cf0347d9f7c7723f9755a3
Harmandeep <just.h4harman@gmail.com>
2024-10-28 18:27:12 +0530

    default redirect to /parent

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\Test;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::redirect('/', '/parent');
+
+Route::get('/test', Test::class);
+
