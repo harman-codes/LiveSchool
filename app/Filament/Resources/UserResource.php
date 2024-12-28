15ab3499631367789285a879ac0241d67e319ec0
Harmandeep <just.h4harman@gmail.com>
2024-12-28 22:34:33 +0530

    more fields added

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -8,6 +8,7 @@ use App\Helpers\Role;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\Split;
 use Filament\Infolists\Components\TextEntry;
@@ -15,7 +16,6 @@ use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 
 class UserResource extends Resource
 {
@@ -23,7 +23,9 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-user-circle';
 
-    protected static ?string $label = 'Teachers';
+    protected static ?string $label = 'Users';
+    protected static ?string $navigationGroup = 'Users';
+    protected static ?int $navigationSort = 1;
 
     public static function getNavigationBadge(): ?string
     {
@@ -76,7 +78,8 @@ class UserResource extends Resource
                             ->dehydrated(fn($state) => filled($state))
                             ->required(fn(string $context): bool => $context === 'create'),
                         Forms\Components\Select::make('role')
-                            ->options(Role::$rolesKeyValuePair),
+                            ->options(Role::$rolesKeyValuePair)
+                        ->required(),
                     ])
             ]);
     }
@@ -85,20 +88,54 @@ class UserResource extends Resource
     {
         return $table
             ->recordUrl(null)
-            ->modifyQueryUsing(function (Builder $query) {
-                //Hide Admin record from the table
-                return $query->where('id', '!=', 1);
-            })
+//            ->modifyQueryUsing(function (Builder $query) {
+//                //Hide Admin record from the table
+//                return $query->where('id', '!=', 1);
+//            })
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('mobile')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email'),
-//                Tables\Columns\TextColumn::make('role'),
                 Tables\Columns\SelectColumn::make('role')
                     ->options(Role::$rolesKeyValuePair)
-                    ->visible(Role::isAdminOrPrincipal()),
+                    ->visible(Role::isAdmin())
+                    ->disabled(fn($record) => $record->id === 1),
+                Tables\Columns\TextColumn::make('showrole')
+                    ->label('Role')
+                    ->getStateUsing(fn($record) => ucwords($record->role))
+                    ->visible(!Role::isAdmin()),
+                Tables\Columns\ToggleColumn::make('is_admin')
+                    ->label('Admin')
+                    ->disabled(fn($record) => $record->id === 1)
+                    ->visible(fn($record) => Role::isAdmin())
+                    ->onColor('success')
+                    ->offColor('danger'),
+                Tables\Columns\IconColumn::make('is_admin_icon')
+                    ->boolean()
+                    ->label('Admin')
+                    ->visible(!Role::isAdmin()),
+                Tables\Columns\ToggleColumn::make('is_active')
+                    ->label('Active')
+                    ->disabled(fn($record) => $record->id === 1)
+                    ->visible(fn($record) => Role::isAdminOrManagementOrPrincipalOrManager())
+                    ->onColor('success')
+                    ->offColor('danger'),
+                Tables\Columns\IconColumn::make('is_active_icon')
+                    ->boolean()
+                    ->label('Active')
+                    ->visible(!Role::isAdminOrManagementOrPrincipalOrManager()),
+                Tables\Columns\ToggleColumn::make('is_available')
+                    ->label('Available')
+                    ->disabled(fn($record) => $record->id === 1)
+                    ->visible(fn($record) => Role::isAdminOrManagementOrPrincipalOrManager())
+                    ->onColor('success')
+                    ->offColor('danger'),
+                Tables\Columns\IconColumn::make('is_available_icon')
+                    ->boolean()
+                    ->label('Available')
+                    ->visible(!Role::isAdminOrManagementOrPrincipalOrManager()),
             ])
             ->filters([
                 //
@@ -138,8 +175,22 @@ class UserResource extends Resource
                         ->schema([
                             TextEntry::make('username'),
                             TextEntry::make('role'),
-                        ])->grow(false)
+                        ])->grow(false),
                 ])->from('md')->columnSpan('full'),
+
+                /*Section Bottom*/
+                Section::make()
+                    ->schema([
+                        IconEntry::make('is_admin')
+                            ->label('Admin')
+                            ->boolean(),
+                        IconEntry::make('is_active')
+                            ->label('Active')
+                            ->boolean(),
+                        IconEntry::make('is_available')
+                            ->label('Available')
+                            ->boolean(),
+                    ])->columns(3)
             ]);
     }
 
@@ -155,7 +206,7 @@ class UserResource extends Resource
     {
         return [
             'index' => Pages\ListUsers::route('/'),
-            'create' => Pages\CreateUser::route('/create'),
+//            'create' => Pages\CreateUser::route('/create'),
 //            'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
