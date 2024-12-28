88f6007ca77e4a6e00a8eda36f4e1695bc73031f
Harmandeep <just.h4harman@gmail.com>
2024-12-28 22:36:20 +0530

    Tabs added to filter data

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,12 +3,32 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Filament\Widgets\CalendarWidget;
+use App\Helpers\Role;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+
+    public function getTabs(): array
+    {
+        $tabsArray = [
+            'all' => Tab::make('All'),
+            'admin' => Tab::make('Admin')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('is_admin', true)),
+        ];
+        foreach (Role::$rolesKeyValuePair as $key => $value) {
+            $tabsArray += [
+                $key => Tab::make($value)
+                    ->modifyQueryUsing(fn(Builder $query) => $query->where('role', $key))
+            ];
+        }
+        return $tabsArray;
+    }
 
     protected function getHeaderActions(): array
     {
