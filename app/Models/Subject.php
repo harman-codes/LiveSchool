20a48aa94abc1e9aaffca6beacd676ccf73eee30
Harmandeep <just.h4harman@gmail.com>
2024-12-28 22:40:18 +0530

    format name

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,8 +10,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Subject extends Model
 {
     use HasFactory;
-
-
 
     public function schoolclasses(): BelongsToMany
     {
@@ -21,5 +20,13 @@ class Subject extends Model
     public function exams(): BelongsToMany
     {
         return $this->belongsToMany(Exam::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => strtolower($value),
+        );
     }
 }
