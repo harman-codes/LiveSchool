719d5e58e3364412360b6c2b6be6690c71cfdd7b
Harmandeep <just.h4harman@gmail.com>
2024-12-28 22:39:41 +0530

    corrections and updated

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\StudentResource\Pages;
 use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Filament\Widgets\SessionyearSelectorWidget;
 use App\Helpers\Notify;
 use App\Helpers\SessionYears;
 use App\Models\Schoolclass;
@@ -13,6 +14,8 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\On;
 
 class StudentResource extends Resource
 {
@@ -22,11 +25,20 @@ class StudentResource extends Resource
 
     protected static ?string $navigationBadgeTooltip = 'Total Students';
 
+    protected static ?string $navigationGroup = 'Students';
+    protected static ?int $navigationSort = 1;
+
     public static function getNavigationBadge(): ?string
     {
         return Student::count();
     }
 
+//    public static function getEloquentQuery(): Builder
+//    {
+//        return parent::getEloquentQuery()->with('studentdetails', function($query) {
+//            return $query->where('sessionyear', '2025-26')->with(['schoolclass']);
+//        });
+//    }
 
 
     public static function table(Table $table): Table
@@ -35,23 +47,67 @@ class StudentResource extends Resource
             ->recordUrl(null)
             ->paginated([10, 25, 50, 100, 'all'])
             ->defaultPaginationPageOption(25)
+            ->modifyQueryUsing(function(Builder $query) {
+                return $query->with('studentdetails', function($query) {
+                    return $query->where('sessionyear', SessionYears::currentSessionYear())->with(['schoolclass']);
+                });
+            })
             ->columns([
                 Tables\Columns\TextColumn::make('name')
-                ->searchable(),
-                Tables\Columns\TextColumn::make('studentdetails.sessionyear')
-                ->label('Session')
-                ->listWithLineBreaks(),
-
-                Tables\Columns\TextColumn::make('studentdetails.schoolclass.classwithsection')
-                    ->label('Class')
-                    ->listWithLineBreaks()
                     ->searchable(),
+                Tables\Columns\TextColumn::make('admissionno')
+                    ->label('Adm No')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('mobile')
+                ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('email')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('fathername')
+                    ->label('Father\'s Name')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('mothername')
+                    ->label('Mother\'s Name')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('address')
+                    ->wrap()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('username')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('studentdetails.sessionyear')
+                    ->label('Session')
+                    ->listWithLineBreaks(),
+                Tables\Columns\TextColumn::make('class')
+                    ->getStateUsing(function ($record) {
+                        return $record->studentdetails()->first()?->schoolclass?->classwithsection;
+                    }),
+//                Tables\Columns\TextColumn::make('sessionyear')
+//                    ->getStateUsing(function ($record) {
+//                        return $record->studentdetails()->first()?->sessionyear;
+//                    }),
+//                Tables\Columns\TextColumn::make('studentdetails.schoolclass.classwithsection')
+//                    ->label('Class')
+//                    ->listWithLineBreaks()
+//                    ->searchable(),
                 Tables\Columns\TextColumn::make('studentdetails.rollno')
                     ->label('Roll No')
                     ->listWithLineBreaks(),
             ])
             ->filters([
                 //--------Filter--------//
+//                Tables\Filters\Filter::make('filtetr')
+//                    ->form([
+//                        Forms\Components\Select::make('sessionyear')
+//                            ->label('Session Year')
+//                            ->options(SessionYears::years())
+//                    ])
+//                    ->query(function (Builder $query, array $data): Builder {
+//                        return $query
+//                            ->when(
+//                                $data['sessionyear'],
+//                                fn (Builder $query): Builder => $query->withWhereHas('studentdetails', function($query) use ($data) {
+//                                    return $query->where('sessionyear', $data['sessionyear'])->with(['schoolclass']);
+//                                }));
+//                    }),
 //                Tables\Filters\Filter::make('sessionyear')
 //                    ->form([
 //                        Forms\Components\Select::make('choosesessionyear')
@@ -61,17 +117,16 @@ class StudentResource extends Resource
 //                            '2025-26' => '2025-26',
 //                            '2026-27' => '2026-27',
 //                        ])
+//                        ->default('2024-25')
 //                    ])->query(function (Builder $query, array $data): Builder {
-//                        $qr = $query->withWhereHas('studentdetails', function($query){
-//                             $query->where('rollno', '99');
-//                         });
-//                        info($qr->toSql());
-//                        return $qr;
+//                        return $query->with('studentdetails', function($query) use ($data) {
+//                                    return $query->where('sessionyear', '2024-25')->with(['schoolclass']);
+//                            });
 //                    }),
                 //------Filter Ends-----//
             ], layout: Tables\Enums\FiltersLayout::AboveContent)
             ->actions([
-                Tables\Actions\Action::make('studentdetails')
+                Tables\Actions\Action::make('studentdetailsassign')
                     ->label('Assign')
                     ->icon('heroicon-o-plus-circle')
                     ->iconButton()
@@ -82,17 +137,18 @@ class StudentResource extends Resource
                                 ->label('Current Session')
                                 ->options(SessionYears::years())
                                 ->default(SessionYears::currentSessionYear())
-                                ->disabled(),
+                                ->disabled()
+                                ->dehydrated(),
                             Forms\Components\Select::make('classwithsection')
                                 ->label('Class (Section)')
                                 ->options(function () {
                                     return Schoolclass::all()->pluck('classwithsection', 'id');
                                 })
-//                                ->default($schoolclass_id)
+                                ->default($record->studentdetails()->first()?->schoolclass?->id)
                                 ->required(),
                             Forms\Components\TextInput::make('rollno')
                                 ->label('Roll No')
-//                                ->default($rollno)
+                                ->default($record->studentdetails()->first()?->rollno)
                                 ->required(),
                         ];
                     })
@@ -100,8 +156,8 @@ class StudentResource extends Resource
                         $updateOrCreateRecord = $record->studentdetails()->updateOrCreate(
                             [
                                 'student_id' => $record->id,
-                                'sessionyear' => SessionYears::currentSessionYear(),
-//                                'sessionyear' => $data['sessionyear']
+//                                'sessionyear' => SessionYears::currentSessionYear(),
+                                'sessionyear' => $data['sessionyear']
                             ],
                             [
                                 'schoolclass_id' => $data['classwithsection'],
@@ -112,8 +168,8 @@ class StudentResource extends Resource
                             Notify::success('Saved Successfully');
                         }
                     }),
-                    Tables\Actions\EditAction::make(),
-                    Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -126,28 +182,93 @@ class StudentResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')->required(),
-                Forms\Components\Select::make('gender')
-                    ->options(['m' => 'Male', 'f' => 'Female', 'o' => 'Other'])->required(),
-                Forms\Components\DatePicker::make('dob'),
-                Forms\Components\TextInput::make('mobile')
-                    ->tel(),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->unique(ignoreRecord: true)
-                    ->required(),
-                Forms\Components\TextInput::make('fathername')
-                    ->label('Father\'s Name'),
-                Forms\Components\TextInput::make('mothername')
-                    ->label('Mother\'s Name'),
-                Forms\Components\Textarea::make('address'),
-                Forms\Components\TextInput::make('username')
-                    ->unique(ignoreRecord: true)
-                    ->required(),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->dehydrated(fn ($state) => filled($state))
-                    ->required(fn(string $context): bool => $context === 'create'),
+                /*Section 1*/
+                Forms\Components\Section::make()
+                    ->columns([
+                        'sm' => 1,
+                        'md' => 2
+                    ])
+                    ->schema([
+                        Forms\Components\TextInput::make('admissionno')
+                            ->label('Admission No'),
+                        Forms\Components\TextInput::make('name')
+                            ->required(),
+                        Forms\Components\Select::make('gender')
+                            ->options(['m' => 'Male', 'f' => 'Female', 'o' => 'Other'])->required(),
+                        Forms\Components\DatePicker::make('dob')
+                            ->label('DOB'),
+                    ]),
+
+                /*Section 2*/
+                Forms\Components\Section::make()
+                    ->columns([
+                        'sm' => 1,
+                        'md' => 2
+                    ])
+                    ->schema([
+                        Forms\Components\TextInput::make('mobile')
+                            ->tel(),
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->unique(ignoreRecord: true)
+                            ->required(),
+                    ]),
+
+                /*Section 3*/
+                Forms\Components\Section::make()
+                    ->columns([
+                        'sm' => 1,
+                        'md' => 2
+                    ])
+                    ->schema([
+                        Forms\Components\TextInput::make('fathername')
+                            ->label('Father\'s Name'),
+                        Forms\Components\TextInput::make('mothername')
+                            ->label('Mother\'s Name'),
+                        Forms\Components\Textarea::make('address'),
+                    ]),
+
+                /*Secction 4*/
+                Forms\Components\Section::make()
+                    ->columns([
+                        'sm' => 1,
+                        'md' => 2
+                    ])
+                    ->schema([
+                        Forms\Components\TextInput::make('username')
+                            ->unique(ignoreRecord: true)
+                            ->required(),
+                        Forms\Components\TextInput::make('password')
+                            ->password()
+                            ->dehydrated(fn($state) => filled($state))
+                            ->required(fn(string $context): bool => $context === 'create'),
+                    ]),
+
+                /*Section 5*/
+//                Forms\Components\Section::make()
+//                    ->columns([
+//                        'sm' => 1,
+//                        'md' => 3
+//                    ])
+//                    ->heading('Optional')
+//                    ->description('You can update these options later')
+//                    ->schema([
+//                        Forms\Components\Select::make('sessionyear')
+//                            ->relationship('studentdetails', 'sessionyear')
+////                            ->options(SessionYears::years())
+//                            ->default(SessionYears::currentSessionYear()),
+//                        Forms\Components\Select::make('studentdetails.student_id')
+//                            ->label('Class (Section)')
+//                            ->relationship('studentdetails', 'schoolclass_id', modifyQueryUsing: fn($query) => $query->orderBy('sort', 'asc'))
+//                        ->options(function(){
+//                            return Schoolclass::all()->pluck('classwithsection', 'id');
+//                        }),
+//                            ->options(function () {
+//                                return Schoolclass::all()->pluck('classwithsection', 'id');
+//                             }),
+//                        Forms\Components\TextInput::make('studentdetails.rollno')
+//                            ->label('Roll No'),
+//                    ]),
             ]);
     }
 
