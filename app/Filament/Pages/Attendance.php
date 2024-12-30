e8d0dba9b8101fcdaf4d985b3d7725a20633a243
Harmandeep <just.h4harman@gmail.com>
2024-12-30 16:52:40 +0530

    changes in table sort. Roll No column is sortable.

--- app/Filament/Pages/Attendance.php
+++ app/Filament/Pages/Attendance.php
@@ -9,11 +9,9 @@ use App\Models\Schoolclass;
 use App\Models\Student;
 use App\Tables\Columns\AttendanceColumn;
 use Carbon\Carbon;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
-use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
@@ -23,6 +21,7 @@ class Attendance extends Page implements HasForms, HasTable
 {
     use InteractsWithTable;
     use InteractsWithForms;
+
     protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
 
     protected static string $view = 'filament.pages.attendance';
@@ -35,11 +34,11 @@ class Attendance extends Page implements HasForms, HasTable
 
     public function updated($property)
     {
-        if($property == 'selectedClass'){
+        if ($property == 'selectedClass') {
             $this->dispatch('class-selected', selectedclass: $this->selectedClass)->to(SelectedClassAttendanceChart::class);
         }
 
-        if($property == 'selectedDate'){
+        if ($property == 'selectedDate') {
             $this->dispatch('date-selected', selecteddate: $this->selectedDate)->to(SelectedClassAttendanceChart::class);
         }
     }
@@ -60,29 +59,37 @@ class Attendance extends Page implements HasForms, HasTable
             ->striped()
             ->emptyStateHeading('Select Options')
             ->emptyStateDescription('Please select class and date to display the attendance.')
-            ->query(function(){
-                if(empty($this->selectedClass)||empty($this->selectedDate)){
+            ->query(function () {
+                if (empty($this->selectedClass) || empty($this->selectedDate)) {
                     return Student::query()->whereNull('id');
                 }
                 return Student::query()->withWhereHas('studentdetails', function ($query) {
                     $query->where('sessionyear', SessionYears::currentSessionYear())->withWhereHas('schoolclass', function ($query) {
-                        $query->where('classwithsection','like', '%'.$this->selectedClass.'%');
+                        $query->where('classwithsection', 'like', '%' . $this->selectedClass . '%');
                     });
                 });
             })
+            ->defaultSort('rollno', 'asc')
             ->columns([
                 TextColumn::make('name')
-                    ->description(function($record){
-                        $rollno = $record->studentdetails?->first()?->rollno ?? '';
+                    ->description(function ($record) {
+//                        $rollno = $record->studentdetails?->first()?->rollno ?? '';
                         $class = $record->studentdetails?->first()?->schoolclass?->classwithsection ?? '';
-                        return $class.' | '.$rollno;
+//                        return $class . ' | ' . $rollno;
+                        return $class . ' | ' .DT::formatDate($this->selectedDate) ;
                     }),
-                TextColumn::make('date')
-                ->state(function(){
-                    return DT::formatDate($this->selectedDate);
-                }),
+                TextColumn::make('rollno')
+                    ->label('Roll No')
+                    ->getStateUsing((function ($record) {
+                        return (int)$record->studentdetails?->first()?->rollno;
+                    }))
+                    ->sortable(),
+//                TextColumn::make('date')
+//                    ->state(function () {
+//                        return DT::formatDate($this->selectedDate);
+//                    }),
                 AttendanceColumn::make('markattendance')
-                ->label('Mark Attendance'),
+                    ->label('Mark Attendance'),
             ])
             ->filters([
                 // ...
