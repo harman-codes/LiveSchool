7577e9d0abe55a41f2dd71c2bfdb0273b0a3738d
Harmandeep <just.h4harman@gmail.com>
2025-01-01 23:30:56 +0530

    Widgets adjustment

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Pages;
 
-use App\Filament\Widgets\CalendarViewOnlyWidget;
 use App\Filament\Widgets\FullSchoolAttendanceChart;
+use App\Filament\Widgets\StatsOverview;
+use App\Filament\Widgets\TempWidget;
+use App\Filament\Widgets\TimeTable;
 use App\Helpers\DT;
 use App\Models\Schoolclass;
 use Filament\Forms\Components\DatePicker;
@@ -15,28 +17,30 @@ class Dashboard extends \Filament\Pages\Dashboard
 {
     use HasFiltersForm;
 
-    public function filtersForm(Form $form): Form
-    {
-        return $form
-            ->schema([
-                DatePicker::make('dateforattendance')
-                    ->label('Date')
-                    ->default(DT::currentDate()),
-                Select::make('classforattendance')
-                    ->label('Class')
-                    ->options(function () {
-                        $classes = Schoolclass::query()->orderBy('sort', 'asc')->pluck('classwithsection', 'classwithsection')->toArray();
-                        return ['wholeschool' => 'School'] + $classes;
-                    })
-                    ->default('wholeschool')
-            ]);
-    }
+//    public function filtersForm(Form $form): Form
+//    {
+//        return $form
+//            ->schema([
+//                DatePicker::make('dateforattendance')
+//                    ->label('Date')
+//                    ->default(DT::currentDate()),
+//                Select::make('classforattendance')
+//                    ->label('Class')
+//                    ->options(function () {
+//                        $classes = Schoolclass::query()->orderBy('sort', 'asc')->pluck('classwithsection', 'classwithsection')->toArray();
+//                        return ['wholeschool' => 'School'] + $classes;
+//                    })
+//                    ->default('wholeschool')
+//            ]);
+//    }
 
     public function getWidgets(): array
     {
         return [
+            StatsOverview::class,
             FullSchoolAttendanceChart::class,
-            CalendarViewOnlyWidget::class,
+            TimeTable::class,
+//            CalendarViewOnlyWidget::class,
         ];
     }
 
@@ -44,7 +48,6 @@ class Dashboard extends \Filament\Pages\Dashboard
     {
         return [
             'md' => 2,
-            'xl' => 2,
         ];
     }
 }
