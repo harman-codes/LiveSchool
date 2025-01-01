6029476090d0abb5394703d5d43131b0dfa1b193
Harmandeep <just.h4harman@gmail.com>
2025-01-01 23:30:07 +0530

    relation with Announcements added

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -64,5 +65,10 @@ class User extends Authenticatable
         return Attribute::make(
             get: fn(mixed $value, array $attributes) => $attributes['is_available'],
         );
+    }
+
+    public function announcements(): HasMany
+    {
+        return $this->hasMany(Announcement::class);
     }
 }
